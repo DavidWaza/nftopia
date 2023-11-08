92045c0b372347f974955751166d83f4d783ef02
David Waza <wazabanga00@gmail.com>
2023-11-08 09:57:11 +0100

    fix collection image alignment

--- src/component/Collection/Collection.tsx
+++ src/component/Collection/Collection.tsx
@@ -5,10 +5,13 @@ import Image from "next/image";
 
 const Collection = () => {
   return (
-    <section className="mt-[10rem] ">
+    <section className="mt-[10rem]">
       <Container>
+        <div className="text-center lg:text-start">
+
         <Title variant="normal">NFTopia Collection</Title>
-        <div className="collection-spiral">
+        </div>
+        <div className="collection-spiral -ml-[5%]">
           <img src="/assets/Frame 209.png" alt="frame" />
         </div>
       </Container>
